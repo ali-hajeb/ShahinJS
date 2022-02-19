7065be1143a11f4d4039398a2ed9309b0c643e50
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-19 22:46:57 +0330

    Implemented category module route

--- backend/api/modules/index.js
+++ backend/api/modules/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./user/routes');
 const adminRoutes = require('./admin/routes');
 const postRoutes = require('./post/routes');
+const categoryRoutes = require('./category/routes');
 
 module.exports = (app) => {
-  app.use('/api/v1/user', userRoutes);
   app.use('/api/v1/admin', adminRoutes);
+  app.use('/api/v1/user', userRoutes);
   app.use('/api/v1/post', postRoutes);
+  app.use('/api/v1/category', categoryRoutes);
 };
