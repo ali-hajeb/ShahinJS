eab89a22412d28f0516ee344b35c9d15fad7a9f1
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-20 23:34:05 +0330

    Added routes for label module

---
+++ backend/api/modules/label/routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const { jwtAdminAuth } = require('../../passport');
+const labelControllers = require('./controllers');
+
+const router = new Router();
+
+router.post('/', jwtAdminAuth, labelControllers.createLabel);
+router.patch('/', jwtAdminAuth, labelControllers.editLabel);
+router.delete('/', jwtAdminAuth, labelControllers.deleteLabel);
+router.get('/', labelControllers.getLabels);
+
+module.exports = router;
