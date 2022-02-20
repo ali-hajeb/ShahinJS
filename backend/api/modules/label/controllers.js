8d7ae8d6fce8009f57e6307ca57115cd33c017f3
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-20 23:33:38 +0330

    Added controllers for label module

---
+++ backend/api/modules/label/controllers.js
@@ -0,0 +1,60 @@
+const labelSchema = require('./model');
+const httpStatus = require('http-status');
+
+const createLabel = async (req, res) => {
+  try {
+    const label = await labelSchema.create(req.body);
+    res.status(httpStatus.CREATED).json(label);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+const editLabel = async (req, res) => {
+  try {
+    const label = await labelSchema.findByIdAndUpdate(req.body.id, req.body, {
+      new: true,
+    });
+    res.status(httpStatus.OK).json(label);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+const deleteLabel = async (req, res) => {
+  try {
+    const label = await labelSchema.findById(req.body.id);
+    if (!label) return res.status(httpStatus.NOT_FOUND).send();
+    await label.remove();
+    res.status(httpStatus.OK).send();
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+const getLabels = async (req, res) => {
+  try {
+    const { filter = {}, limit = '*' } = req.body;
+    const label = await labelSchema.find({ filter, limit });
+    res.status(httpStatus.OK).json(label);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+// const function = async (req, res) => {
+//   try {
+
+//   } catch (error) {
+
+//   }
+// }
+
+module.exports = {
+  createLabel,
+  editLabel,
+  deleteLabel,
+  getLabels,
+};
