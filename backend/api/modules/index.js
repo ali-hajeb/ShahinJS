700c4eee57bb90cecb8df1d55444ddeab0efa024
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-20 23:34:58 +0330

    Implemented label module route

--- backend/api/modules/index.js
+++ backend/api/modules/index.js
@@ -2,10 +2,12 @@ const userRoutes = require('./user/routes');
 const adminRoutes = require('./admin/routes');
 const postRoutes = require('./post/routes');
 const categoryRoutes = require('./category/routes');
+const labelRoutes = require('./label/routes');
 
 module.exports = (app) => {
   app.use('/api/v1/admin', adminRoutes);
   app.use('/api/v1/user', userRoutes);
   app.use('/api/v1/post', postRoutes);
   app.use('/api/v1/category', categoryRoutes);
+  app.use('/api/v1/label', labelRoutes);
 };
