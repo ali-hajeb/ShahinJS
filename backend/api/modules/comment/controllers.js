ee770b0ab4a56f7e428e3ded2df5de3841af5c15
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-21 22:18:56 +0330

    Added controllers for comment module

---
+++ backend/api/modules/comment/controllers.js
@@ -0,0 +1,30 @@
+const commentSchema = require('./model');
+
+const writeComment = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const editComment = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const deleteComment = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const getCommentsByUserId = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+const getCommentById = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  writeComment,
+  editComment,
+  deleteComment,
+  getCommentsByUserId,
+  getCommentById,
+};
