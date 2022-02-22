29b9490f2171b0e1ba5ef136cc686577c9fc735c
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-22 20:19:14 +0330

    Added new functions (in prog.)

--- backend/api/modules/comment/controllers.js
+++ backend/api/modules/comment/controllers.js
@@ -1,11 +1,19 @@
 const commentSchema = require('./model');
+const httpStatus = require('http-status');
 
 const writeComment = async (req, res) => {
   try {
-  } catch (error) {}
+    const comment = await commentSchema.create(req.body);
+    res.status(httpStatus.CREATED).json(comment);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
 };
 const editComment = async (req, res) => {
   try {
+    // const comment = await commentSchema.findById(req.body.id);
+    // To be planned
   } catch (error) {}
 };
 const deleteComment = async (req, res) => {
@@ -16,9 +24,27 @@ const getCommentsByUserId = async (req, res) => {
   try {
   } catch (error) {}
 };
+const getCommentsByPostId = async (req, res) => {
+  try {
+    const comment = await commentSchema.find({ postId: req.params.id });
+    comment
+      ? res.status(httpStatus.OK).json(comment)
+      : res.status(httpStatus.NOT_FOUND);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
 const getCommentById = async (req, res) => {
   try {
-  } catch (error) {}
+    const comment = await commentSchema.findById(req.body.id);
+    comment
+      ? res.status(httpStatus.OK).json(comment)
+      : res.status(httpStatus.NOT_FOUND);
+  } catch (error) {
+    console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
 };
 
 module.exports = {
@@ -26,5 +52,6 @@ module.exports = {
   editComment,
   deleteComment,
   getCommentsByUserId,
+  getCommentsByPostId,
   getCommentById,
 };
