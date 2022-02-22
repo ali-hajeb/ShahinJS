5f7987f1e54a078d71b4be40d49c7617bfdf34fb
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-22 20:19:57 +0330

    Added new fields, methods & middlewares (in prog.)

--- backend/api/modules/comment/model.js
+++ backend/api/modules/comment/model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const postSchema = require('../post/model');
 const validator = require('validator');
 
 const commentSchema = new Schema(
@@ -25,8 +26,55 @@ const commentSchema = new Schema(
       required: true,
       default: 'PROCESSING',
     },
+    postId: { type: Schema.Types.ObjectId, ref: 'Posts', required: true },
+    repliedTo: {
+      type: Schema.Types.ObjectId,
+      ref: 'Comments',
+    },
+    replies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comments',
+      },
+    ],
   },
   { timestamps: true },
 );
+
+commentSchema.post('save', function (err, doc, next) {
+  if (err)
+    return next({
+      status: httpStatus.BAD_REQUEST,
+      err,
+    });
+
+  this.addCommentToPost();
+  next();
+});
+
+commentSchema.methods = {
+  setCommentStatus(status = 'PROCESSING' || 'APPROVED' || 'DELETED') {
+    this.status = status;
+  },
+  addCommentToPost() {
+    return postSchema.findByIdAndUpdate(
+      this.postId,
+      { $push: { comments: this._id } },
+      { new: true },
+    );
+  },
+  removeCommentFromPost() {
+    return postSchema.findByIdAndUpdate(
+      this.postId,
+      { $pull: { comments: this._id } },
+      { new: true },
+    );
+  },
+};
+
+commentSchema.statics = {
+  addCommentToPost(commentId, postId) {},
+  removeCommentFromPost(commentId, postId) {},
+};
 
 module.exports = model('Comments', commentSchema, 'Comments');
