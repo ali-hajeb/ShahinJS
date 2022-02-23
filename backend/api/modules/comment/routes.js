1975505f154bb0de20d94c142b914a1f399adb2f
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-23 21:44:27 +0330

    Set up a new route

--- backend/api/modules/comment/routes.js
+++ backend/api/modules/comment/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { jwtAdminAuth, jwtUserAuth } = require('../../passport/index');
 const commentController = require('./controllers');
 
 const router = new Router();
@@ -7,6 +8,7 @@ router.post('/', commentController.writeComment);
 router.patch('/', commentController.editComment);
 router.delete('/', commentController.deleteComment);
 router.get('/', commentController.getCommentById);
+router.post('/status/', jwtAdminAuth, commentController.changeCommentStatus);
 router.get('/user/:id', commentController.getCommentsByUserId);
 router.get('/post/:id', commentController.getCommentsByPostId);
 
