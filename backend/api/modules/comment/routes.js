49ddcdd9003ef3504c95ec1f5f485ed0708e4671
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-24 20:12:22 +0330

    Re-routed some functions

--- backend/api/modules/comment/routes.js
+++ backend/api/modules/comment/routes.js
@@ -7,9 +7,7 @@ const router = new Router();
 router.post('/', commentController.writeComment);
 router.patch('/', commentController.editComment);
 router.delete('/', commentController.deleteComment);
-router.get('/', commentController.getCommentById);
+router.get('/', commentController.getComments);
 router.post('/status/', jwtAdminAuth, commentController.changeCommentStatus);
-router.get('/user/:id', commentController.getCommentsByUserId);
-router.get('/post/:id', commentController.getCommentsByPostId);
 
 module.exports = router;
