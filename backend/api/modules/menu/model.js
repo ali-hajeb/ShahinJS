7dbebaee36bfbb8de91712e82f84c2bf738afc3c
ali-hajeb <ali.hajeb@hotmail.com>
2022-02-26 23:23:40 +0330

    Added menu model

---
+++ backend/api/modules/menu/model.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const menuSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['NAV', 'SIDE'],
+    require: true,
+  },
+  title: String,
+  items: [{ title: String, href: String, className: String, icon: String }],
+  visibility: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+menuSchema.statics = {};
+
+module.exports = model('Menus', menuSchema, 'Menus');
